1552f474a1a069d54b30b66d3e722961e8d9d1a3
Joscha <joschavonandrian@gmail.com>
2022-10-05 00:15:40 +0200

    fix zonetypes

--- bhoptimer.py
+++ bhoptimer.py
@@ -16,7 +16,17 @@ print(len(maps))
 
 
 def add_maptiers():
-    maptiers = 'INSERT INTO `maptiers` VALUES '
+    maptiers = """
+DROP TABLE IF EXISTS `maptiers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `maptiers` (
+  `map` varchar(255) NOT NULL,
+  `tier` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`map`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `maptiers` VALUES """
 
     for m in maps:
         item = [value for value in ck_maptier if m in value][0]
@@ -32,7 +42,32 @@ def add_maptiers():
 
 
 def add_zones():
-    mapzones = 'INSERT INTO `mapzones` VALUES '
+    mapzones = """
+DROP TABLE IF EXISTS `mapzones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mapzones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `map` varchar(255) NOT NULL,
+  `type` int(11) DEFAULT NULL,
+  `track` tinyint(4) NOT NULL,
+  `corner1_x` float DEFAULT NULL,
+  `corner1_y` float DEFAULT NULL,
+  `corner1_z` float DEFAULT NULL,
+  `corner2_x` float DEFAULT NULL,
+  `corner2_y` float DEFAULT NULL,
+  `corner2_z` float DEFAULT NULL,
+  `destination_x` float NOT NULL DEFAULT '0',
+  `destination_y` float NOT NULL DEFAULT '0',
+  `destination_z` float NOT NULL DEFAULT '0',
+  `flags` int(11) NOT NULL DEFAULT '0',
+  `data` int(11) NOT NULL DEFAULT '0',
+  `form` tinyint(4) DEFAULT NULL,
+  `target` varchar(63) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `mapzones` VALUES """
     i = 0
     for m in maps:
 
@@ -40,8 +75,11 @@ def add_zones():
         if items:
             for item in items:
                 i += 1
-                mapzones += (
-                    f"('{i}', {item[0]}, {item[2]}, {item[3]}, {item[4]}, {item[5]}, {item[6]}, {item[7]}, {item[8]}, {item[9]} ),")
+                # map zonetypes surftimer (index) - bhoptimer (value)
+                zonetypes = [3, 0, 1, 12, -1, -1, 2, -1, -1]
+                if item[2] < 9 and zonetypes[item[2]] != -1:
+                    mapzones += (
+                        f"({i}, '{item[0]}', {zonetypes[item[2]]}, {item[3]}, {item[4]}, {item[5]}, {item[6]}, {item[7]}, {item[8]}, {item[9]}, 0, 0, 0, 0, 0, 0, '' ),")
 
     mapzones = mapzones[:-1]+';'
 
@@ -49,4 +87,5 @@ def add_zones():
         f.write(mapzones)
 
 
+add_maptiers()
 add_zones()
